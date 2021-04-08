e1f8464418c5f78acfb5c8c6877d0672a9625e12
itamarerenberg <59229663+itamarerenberg@users.noreply.github.com>
2021-04-08 21:55:53 +0300

    belong to last commit

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -1,15 +1,48 @@
 package geometries;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import primitives.Point3D;
 import primitives.Ray;
 
+/**
+ * list of Intersectable objects as a composite object
+ */
 public class Geometries implements Intersectable {
-
+	private LinkedList<Intersectable> components;
+	
+	/**
+	 * Default constructor:
+	 * initialize the class's components list to be an empty list
+	 */
+	public Geometries() {
+		components = new LinkedList<Intersectable>();
+	}
+	
+	/**
+	 * initializes the list with the values in 'geometries'
+	 * @param geometries the values to initialize the class's list with
+	 */
+	public Geometries(Intersectable... geometries) {
+		components = new LinkedList<Intersectable>();
+		for (Intersectable intersectable : geometries) {
+			components.add(intersectable);
+		}
+	}
+	
+	/**
+	 * adds a collection of geometries to the components list
+	 * @param geometries geometries to add to the components list
+	 */
+	public void add(Intersectable...geometries) {
+		for (Intersectable intersectable : geometries) {
+			components.add(intersectable);
+		}
+	}
+	
 	@Override
 	public List<Point3D> findIntersections(Ray ray) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
