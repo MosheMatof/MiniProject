3192cb4fb6315b41aab0770e49d5c3622e23ed51
msm770 <38422044+msm770@users.noreply.github.com>
2021-04-23 03:27:47 +0300

    camera class

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -3,8 +3,7 @@
  */
 package elements;
 
-import primitives.Point3D;
-import primitives.Vector;
+import primitives.*;
 
 /**
  * the point of view the scene
@@ -16,16 +15,62 @@ public class Camera {
 	private Vector vUp;
 	private Vector vTo;
 	private Vector vRight;
+	private double width;
+	private double height;
+	private double dis;
 
-	public Camera(Point3D p0, Vector vUp, Vector vTo, double width, double height, double dis) {
+	/**
+	 * camera constructor
+	 * @param p0 the location point of the camera
+	 * @param vUp the up direction of the camera 
+	 * @param vTo the up direction of the camera
+	 */
+	public Camera(Point3D p0, Vector vUp, Vector vTo) {
 		super();
 		this.p0 = p0;
-		this.vUp = vUp;
-		this.vTo = vTo;
-		this.width = width;
-		this.height = height;
-		this.dis = dis;
+		this.vUp = vUp.normalize();
+		this.vTo = vTo.normalize();
+		//if the vectors are orthogonal -> generates the right vector
+		if (vUp.dotProduct(vTo) == 0) {
+			this.vRight = vTo.crossProduct(vUp);//need to normalize?
+		}
+		else {
+			throw new IllegalArgumentException("the vTo and vUp aren't orthogonal");
+		}
+		
 	}
+	/**
+	 * setting the view plane size of the camera
+	 * @param width the width of the view plane
+	 * @param height the height of the view plane
+	 * @return this camera
+	 */
+	public Camera setViewPlaneSize(double width, double height) {
+		this.height = height;
+		this.width = width;
+		return this;
+	}
+	/**
+	 * setting view plane distance from the camera
+	 * @param distance the distance of the view plane from the camera
+	 * @return this camera
+	 */
+	public Camera setDistance(double distance) {
+		this.dis = distance;
+		return this;
+	}
+	/**
+	 * 
+	 * @param nX
+	 * @param nY
+	 * @param j
+	 * @param i
+	 * @return
+	 */
+	public Ray constructRayThroughPixel(int nX, int nY, int j, int i){
+		return null;
+	}
+	
 	public Point3D getP0() {
 		return p0;
 	}
@@ -47,7 +92,5 @@ public class Camera {
 	public double getDis() {
 		return dis;
 	}
-	private double width;
-	private double height;
-	private double dis;
+	
 }
