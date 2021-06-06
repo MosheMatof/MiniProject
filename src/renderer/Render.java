33dd1f9d0dd4c58f44ab3a8c6bf15a8ece6f5a27
msm770 <38422044+msm770@users.noreply.github.com>
2021-06-06 12:37:03 +0300

    Threads

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -12,11 +12,173 @@ import primitives.Ray;
  * Rendering the image from the scene
  */
 public class Render {
-	private static final double MAX_VARIANCE = 2;
 	private ImageWriter imageWriter;
 	private Camera camera;
 	private RayTracerBase rayTracer;
-	private int kA = 5;
+	private int kA = 1;
+
+	private static final String RESOURCE_ERROR = "Renderer resource not set";
+	private static final String RENDER_CLASS = "Render";
+	private static final String IMAGE_WRITER_COMPONENT = "Image writer";
+	private static final String CAMERA_COMPONENT = "Camera";
+	private static final String RAY_TRACER_COMPONENT = "Ray tracer";
+
+	private int threadsCount = 0;
+	private static final int SPARE_THREADS = 2; // Spare threads if trying to use all the cores
+	private static final double MAX_VARIANCE = 2;
+	private boolean print = false; // printing progress percentage
+
+	/**
+	 * Set multi-threading <br>
+	 * - if the parameter is 0 - number of cores less 2 is taken
+	 * 
+	 * @param threads number of threads
+	 * @return the Render object itself
+	 */
+	public Render setMultithreading(int threads) {
+		if (threads < 0)
+			throw new IllegalArgumentException("Multithreading parameter must be 0 or higher");
+		if (threads != 0)
+			this.threadsCount = threads;
+		else {
+			int cores = Runtime.getRuntime().availableProcessors() - SPARE_THREADS;
+			this.threadsCount = cores <= 2 ? 1 : cores;
+		}
+		return this;
+	}
+
+	/**
+	 * Set debug printing on
+	 * 
+	 * @return the Render object itself
+	 */
+	public Render setDebugPrint() {
+		print = true;
+		return this;
+	}
+
+	/**
+	 * Pixel is an internal helper class whose objects are associated with a Render
+	 * object that they are generated in scope of. It is used for multithreading in
+	 * the Renderer and for follow up its progress.<br/>
+	 * There is a main follow up object and several secondary objects - one in each
+	 * thread.
+	 * 
+	 * @author Dan
+	 *
+	 */
+	private class Pixel {
+		private long maxRows = 0;
+		private long maxCols = 0;
+		private long pixels = 0;
+		public volatile int row = 0;
+		public volatile int col = -1;
+		private long counter = 0;
+		private int percents = 0;
+		private long nextCounter = 0;
+
+		/**
+		 * The constructor for initializing the main follow up Pixel object
+		 * 
+		 * @param maxRows the amount of pixel rows
+		 * @param maxCols the amount of pixel columns
+		 */
+		public Pixel(int maxRows, int maxCols) {
+			this.maxRows = maxRows;
+			this.maxCols = maxCols;
+			this.pixels = (long) maxRows * maxCols;
+			this.nextCounter = this.pixels / 100;
+			if (Render.this.print)
+				System.out.printf("\r %02d%%", this.percents);
+		}
+
+		/**
+		 * Default constructor for secondary Pixel objects
+		 */
+		public Pixel() {
+		}
+
+		/**
+		 * Internal function for thread-safe manipulating of main follow up Pixel object
+		 * - this function is critical section for all the threads, and main Pixel
+		 * object data is the shared data of this critical section.<br/>
+		 * The function provides next pixel number each call.
+		 * 
+		 * @param target target secondary Pixel object to copy the row/column of the
+		 *               next pixel
+		 * @return the progress percentage for follow up: if it is 0 - nothing to print,
+		 *         if it is -1 - the task is finished, any other value - the progress
+		 *         percentage (only when it changes)
+		 */
+		private synchronized int nextP(Pixel target) {
+			++col;
+			++this.counter;
+			if (col < this.maxCols) {
+				target.row = this.row;
+				target.col = this.col;
+				if (Render.this.print && this.counter == this.nextCounter) {
+					++this.percents;
+					this.nextCounter = this.pixels * (this.percents + 1) / 100;
+					return this.percents;
+				}
+				return 0;
+			}
+			++row;
+			if (row < this.maxRows) {
+				col = 0;
+				target.row = this.row;
+				target.col = this.col;
+				if (Render.this.print && this.counter == this.nextCounter) {
+					++this.percents;
+					this.nextCounter = this.pixels * (this.percents + 1) / 100;
+					return this.percents;
+				}
+				return 0;
+			}
+			return -1;
+		}
+
+		/**
+		 * Public function for getting next pixel number into secondary Pixel object.
+		 * The function prints also progress percentage in the console window.
+		 * 
+		 * @param target target secondary Pixel object to copy the row/column of the
+		 *               next pixel
+		 * @return true if the work still in progress, -1 if it's done
+		 */
+		public boolean nextPixel(Pixel target) {
+			int percent = nextP(target);
+			if (Render.this.print && percent > 0)
+				synchronized (this) {
+					notifyAll();
+				}
+			if (percent >= 0)
+				return true;
+			if (Render.this.print)
+				synchronized (this) {
+					notifyAll();
+				}
+			return false;
+		}
+
+		/**
+		 * Debug print of progress percentage - must be run from the main thread
+		 */
+		public void print() {
+			if (Render.this.print)
+				while (this.percents < 100)
+					try {
+						synchronized (this) {
+							wait();
+						}
+						System.out.printf("\r %02d%%", this.percents);
+						System.out.flush();
+					} catch (Exception e) {
+					}
+		}
+	}
+
+
 	/**
 	 * set the imageWriter of the render
 	 * @param imageWriter the imageWriter for the render
@@ -57,65 +219,78 @@ public class Render {
 		this.kA = kA;
 		return this;
 	}
+/**
+	 * This function renders image's pixel color map from the scene included with
+	 * the Renderer object - with multi-threading
+	 */
+	private void renderImageThreaded() {
+		final int nX = imageWriter.getNx();
+		final int nY = imageWriter.getNy();
+		final Pixel thePixel = new Pixel(nY, nX);
+		// Generate threads
+		Thread[] threads = new Thread[threadsCount];
+		for (int i = threadsCount - 1; i >= 0; --i) {
+			threads[i] = new Thread(() -> {
+				Pixel pixel = new Pixel();
+				while (thePixel.nextPixel(pixel))
+					renderImage(nX, nY, pixel.col, pixel.row);
+			});
+		}
+		// Start threads
+		for (Thread thread : threads)
+			thread.start();
 
+		// Print percents on the console
+		thePixel.print();
+
+		// Ensure all threads have finished
+		for (Thread thread : threads)
+			try {
+				thread.join();
+			} catch (Exception e) {
+			}
+
+		if (print)
+			System.out.print("\r100%");
+	}
 	/** 
 	 * Rendering the image by imageWriter according to the rayTracer and camera 
 	 * @param rays the number of rays per pixel
 	 */
-	public void renderImage() {
-		var render = "Render";
-		if (imageWriter == null)
-			throw new MissingResourceException("imageWriter is null ", render, "imageWriter");
-		if (rayTracer == null)
-			throw new MissingResourceException("rayTracer is null ", render, "rayTracer");
-		if (camera == null)
-			throw new MissingResourceException("camera is null ", render, "camera");
+	private void renderImage(int nX, int nY, int j, int i) {
 
-		int nX = imageWriter.getNx();
-		int nY = imageWriter.getNy();
 		BlackBoard sampleBoard = BlackBoard.sempleSquare(1);
 		BlackBoard randomBoard = BlackBoard.squareRandom(1, kA);
 		if (kA > 4) {
-			for (int i = 0; i < nY; i++) {
-				for (int j = 0; j < nX; j++) {
-					//get sample of 5 colors
-					List<Ray> sampleRays = camera.constructBeamThroughPixel(sampleBoard, nX, nY, i, j);
-					List<Color> sampleColors = new LinkedList<>();
-					for (Ray ray : sampleRays) {
-						sampleColors.add(rayTracer.traceRay(ray));
-					}
-					Color sampleAvg = new Color(sampleColors);
+			//get sample of 5 colors
+			List<Ray> sampleRays = camera.constructBeamThroughPixel(sampleBoard, nX, nY, i, j);
+			List<Color> sampleColors = new LinkedList<>();
+			for (Ray ray : sampleRays) {
+				sampleColors.add(rayTracer.traceRay(ray));
+			}
+			Color sampleAvg = new Color(sampleColors);
 
-					if (calcVarianceColors(sampleColors, sampleAvg) < MAX_VARIANCE) {
-						imageWriter.writePixel(i, j, sampleAvg);
-					} else {
-						List<Ray> rays = camera.constructBeamThroughPixel(randomBoard, nX, nY, i, j);
-						List<Color> colors = new LinkedList<>();
-						for (Ray ray : rays) {
-							colors.add(rayTracer.traceRay(ray));
-						}
-						colors.addAll(sampleColors);
-						Color AvgColor = new Color(colors);
-						imageWriter.writePixel(i, j, AvgColor);
-					}			
+			if (calcVarianceColors(sampleColors, sampleAvg) < MAX_VARIANCE) {
+				imageWriter.writePixel(i, j, sampleAvg);
+			} else {
+				List<Ray> rays = camera.constructBeamThroughPixel(randomBoard, nX, nY, i, j);
+				List<Color> colors = new LinkedList<>();
+				for (Ray ray : rays) {
+					colors.add(rayTracer.traceRay(ray));
 				}
+				colors.addAll(sampleColors);
+				Color AvgColor = new Color(colors);
+				imageWriter.writePixel(i, j, AvgColor);
 			}
 		}
 		else{
-			for (int i = 0; i < nY; i++) {
-				for (int j = 0; j < nX; j++) {
-					if (j == 90 && i == 120) {
-						int a = 1;
-					}
-					List<Ray> rays = camera.constructBeamThroughPixel(randomBoard, nX, nY, i, j);
-					List<Color> colors = new LinkedList<>();
-					for (Ray ray : rays) {
-						colors.add(rayTracer.traceRay(ray));
-					}
-					Color AvgColor = new Color(colors);
-					imageWriter.writePixel(i, j, AvgColor);
-				}
+			List<Ray> rays = camera.constructBeamThroughPixel(randomBoard, nX, nY, i, j);
+			List<Color> colors = new LinkedList<>();
+			for (Ray ray : rays) {
+				colors.add(rayTracer.traceRay(ray));
 			}
+			Color AvgColor = new Color(colors);
+			imageWriter.writePixel(i, j, AvgColor);
 		}		
 	}
 
@@ -189,6 +364,24 @@ public class Render {
 		}
 	}
 
+	public void renderImage() {
+		var render = "Render";
+		if (imageWriter == null)
+			throw new MissingResourceException("imageWriter is null ", render, "imageWriter");
+		if (rayTracer == null)
+			throw new MissingResourceException("rayTracer is null ", render, "rayTracer");
+		if (camera == null)
+			throw new MissingResourceException("camera is null ", render, "camera");
+
+		final int nX = imageWriter.getNx();
+		final int nY = imageWriter.getNy();
+		if (threadsCount == 0)
+			for (int i = 0; i < nY; ++i)
+				for (int j = 0; j < nX; ++j)
+					renderImage(nX, nY, j, i);
+		else
+			renderImageThreaded();
+		}
 	/**
 	 * Generates the image
 	 */
